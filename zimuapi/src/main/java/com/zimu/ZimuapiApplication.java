94da7279c0a0004a190ff8af2a2255f5345d0cbd
yangkun <youngkun@qq.com>
2018-07-07 06:29:04 +0800

    x

---
+++ zimuapi/src/main/java/com/zimu/ZimuapiApplication.java
@@ -0,0 +1,12 @@
+package com.zimu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimuapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZimuapiApplication.class, args);
+    }
+}
---
+++ zimuapi/src/test/java/com/zimu/ZimuapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zimu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZimuapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
