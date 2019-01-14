a6e010fb6c5b35d852e2c40e534c96cc08366543
yangkun <youngkun@qq.com>
2019-01-14 17:43:46 +0800

    x

--- zimu/src/main/resources/static/js/home.js
+++ zimu/src/main/resources/static/js/home.js
@@ -1,4 +1,6 @@
 var tabpanel;
+var search = "";
+var initFlag = false;
 $(document).ready(function () {
 
     tabpanel = new TabPanel({
@@ -14,29 +16,45 @@ $(document).ready(function () {
     $("nav li a").click(function () {
         var dataHref = $(this).attr("data-href");
         if (dataHref == null || dataHref === '' || dataHref === 'javascript:void(0)') {
+            initFlag = false;
             return;
         }
-        addTabPanel($(this));
+
+        if (initFlag) {
+            var hash = location.hash;
+            if (hash != null && hash !== '') {
+                var w = hash.indexOf('?');
+                if (w > 1) {
+                    dataHref = dataHref + hash.substring(w);
+                }
+            }
+        }
+
+        var title = $(this).attr("title");
+        $("title").html(title);
+        var dataTabId = $(this).attr("data-tab-id");
+        var html = '<iframe id="' + dataTabId + '-frame" src="' + dataHref + '" width="100%" height="100%" frameborder="0"></iframe>';
+        var tabitem = {id: dataTabId, title: title, html: html, closable: true};
+        tabpanel.addTab(tabitem);
         window.history.pushState("", "", "#" + dataHref);//添加路由
+        initFlag = false;
     });
 
     var hash = location.hash;
     if (hash != null && hash !== '') {
-        hash = hash.substring(1);
+        var w = hash.indexOf('?');
+        if (w != -1) {
+            hash = hash.substring(1, w);
+        } else {
+            hash = hash.substring(1);
+        }
+
         var a = $("[data-href='" + hash + "']");
         if (a != null && a.length > 0) {
+            initFlag = true;
             $(a[0]).click();
         }
     }
 
 
 });
-
-function addTabPanel(item) {
-    var title = item.attr("title");
-    var dataTabId = item.attr("data-tab-id");
-    var dataHref = item.attr("data-href");
-    var html = '<iframe id="' + dataTabId + '-frame" src="' + dataHref + '" width="100%" height="100%" frameborder="0"></iframe>';
-    var tabitem = {id: dataTabId, title: title, html: html, closable: true};
-    tabpanel.addTab(tabitem);
-}
