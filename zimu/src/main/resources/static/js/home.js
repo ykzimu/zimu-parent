b53c03bcffeb71105873b7181605c70897d9715c
yangkun <youngkun@qq.com>
2019-01-17 12:05:43 +0800

    x

--- zimu/src/main/resources/static/js/home.js
+++ zimu/src/main/resources/static/js/home.js
@@ -19,53 +19,25 @@ $(document).ready(function () {
         scrolled: true,
         //maxLength : 10,
         items: [
-            {id: 'tabpanel-home', title: '首页', html: '首页', closable: false}
+            {id: 'tabpanel-5', title: '主页', html: '主页', closable: false}
         ]
     });
 
     $("nav li a").click(function () {
         var dataHref = $(this).attr("data-href");
         if (dataHref == null || dataHref === '' || dataHref === 'javascript:void(0)') {
-            initFlag = false;
             return;
         }
-
-        if (initFlag) {
-            var hash = location.hash;
-            if (hash != null && hash !== '') {
-                var w = hash.indexOf('?');
-                if (w !== -1) {
-                    dataHref = dataHref + hash.substring(w);
-                }
-            }
-        }
-
         var title = $(this).attr("title");
         $("title").html(title);
         var dataTabId = $(this).attr("data-tab-id");
         var html = '<iframe id="' + dataTabId + 'Frame" src="' + dataHref + '" width="100%" height="100%" frameborder="0" onload="iframeOnload(this)"></iframe>';
         var tabitem = {id: dataTabId, title: title, html: html, closable: true};
         tabpanel.addTab(tabitem);
-        window.history.pushState("", "", "#" + dataHref);//添加路由
-        initFlag = false;
-
+        window.history.replaceState("", "", "#" + dataHref);//添加路由
     });
 
-    var hash = location.hash;
-    if (hash != null && hash !== '') {
-        var w = hash.indexOf('?');
-        if (w !== -1) {
-            hash = hash.substring(1, w);
-        } else {
-            hash = hash.substring(1);
-        }
-
-        var a = $("[data-href='" + hash + "']");
-        if (a != null && a.length > 0) {
-            initFlag = true;
-            $(a[0]).click();
-        }
-    }
+    initWithHash();
 
     $("#fullScreen").click(function () {
         if (screenfull.enabled) {
@@ -194,5 +166,25 @@ function iframeOnload(item) {
     if (minH === '0px') {
         $("#mainContent").height(htmlH - headerH);
         tabpanel.resize();
+    }
+}
+
+/**
+ * 首次进入时，带有hash
+ */
+function initWithHash() {
+    var hash = location.hash;
+    if (hash != null && hash !== '') {
+        var w = hash.indexOf('?');
+        if (w !== -1) {
+            hash = hash.substring(1, w);
+        } else {
+            hash = hash.substring(1);
+        }
+        hash = hash.split("#")[0];
+        var a = $("[data-href='" + hash + "']");
+        if (a != null && a.length > 0) {
+            $(a[0]).click();
+        }
     }
 }
