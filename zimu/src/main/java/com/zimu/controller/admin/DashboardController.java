b067158fe42144daa44361456a14763ee43eedbf
yangkun <youngkun@qq.com>
2019-01-21 11:51:36 +0800

    x

--- zimu/src/main/java/com/zimu/controller/admin/DashboardController.java
+++ zimu/src/main/java/com/zimu/controller/admin/DashboardController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 仪表盘
+ *
+ * @author yk
+ */
 @Controller
 @RequestMapping("/admin/dashboard")
 public class DashboardController {
