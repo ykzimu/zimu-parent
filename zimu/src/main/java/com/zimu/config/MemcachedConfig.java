74dcc0ff789534ef5f0ae939915a4d2983d5126f
yangkun <youngkun@qq.com>
2019-01-23 14:09:41 +0800

    x

--- zimu/src/main/java/com/zimu/config/MemcachedConfig.java
+++ zimu/src/main/java/com/zimu/config/MemcachedConfig.java
@@ -7,8 +7,10 @@ import net.spy.memcached.spring.MemcachedClientFactoryBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 @ConfigurationProperties(prefix = "memcached")
 @Configuration
 public class MemcachedConfig {
