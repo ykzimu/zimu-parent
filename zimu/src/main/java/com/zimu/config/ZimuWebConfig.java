6086de8cec11a3490883440ee5c1cb1fe7d2b2f3
yangkun <youngkun@qq.com>
2019-03-06 18:11:38 +0800

    x

--- zimu/src/main/java/com/zimu/config/ZimuWebConfig.java
+++ zimu/src/main/java/com/zimu/config/ZimuWebConfig.java
@@ -3,27 +3,12 @@ package com.zimu.config;
 import com.zimu.interceptor.HttpServletInterceptor;
 import com.zimu.listener.MuziServletContextListener;
 import com.zimu.resolver.DataTablesHandlerMethodArgumentResolver;
-import org.jasig.cas.client.session.SingleSignOutFilter;
-import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
-import org.jasig.cas.client.util.HttpServletRequestWrapperFilter;
-import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
-import org.jasig.cas.client.validation.Cas30ServiceTicketValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.cas.ServiceProperties;
-import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
-import org.springframework.security.cas.authentication.CasAuthenticationProvider;
-import org.springframework.security.cas.web.CasAuthenticationEntryPoint;
-import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -36,12 +21,6 @@ public class ZimuWebConfig implements WebMvcConfigurer {
 
     @Autowired
     private WebMvcProperties webMvcProperties;
-
-    @Autowired
-    private CasServerConfig casServerConfig;
-
-    @Autowired
-    private CasServiceConfig casServiceConfig;
 
     /**
      * 自定义拦截器
@@ -57,90 +36,12 @@ public class ZimuWebConfig implements WebMvcConfigurer {
         resolvers.add(new DataTablesHandlerMethodArgumentResolver());
     }
 
-    /**
-     * 添加监听器
-     *
-     * @return RequestContextListener
-     */
-    @Bean
-    public ServletListenerRegistrationBean<RequestContextListener> addRequestContextListener() {
-        ServletListenerRegistrationBean<RequestContextListener> servletListenerRegistrationBean = new ServletListenerRegistrationBean<>();
-        servletListenerRegistrationBean.setListener(new RequestContextListener());
-        return servletListenerRegistrationBean;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<SingleSignOutHttpSessionListener> addSingleSignOutHttpSessionListener() {
-        ServletListenerRegistrationBean<SingleSignOutHttpSessionListener> servletListenerRegistrationBean = new ServletListenerRegistrationBean<>();
-        servletListenerRegistrationBean.setListener(new SingleSignOutHttpSessionListener());
-        return servletListenerRegistrationBean;
-    }
 
     @Bean
     public ServletListenerRegistrationBean<MuziServletContextListener> addMuziServletContextListener() {
         ServletListenerRegistrationBean<MuziServletContextListener> servletListenerRegistrationBean = new ServletListenerRegistrationBean<>();
         servletListenerRegistrationBean.setListener(new MuziServletContextListener());
         return servletListenerRegistrationBean;
-    }
-
-    @Bean
-    public ServiceProperties serviceProperties() {
-        ServiceProperties serviceProperties = new ServiceProperties();
-        serviceProperties.setService(this.casServiceConfig.getHost() + this.casServiceConfig.getLogin());
-        serviceProperties.setSendRenew(this.casServiceConfig.getSendRenew());
-        serviceProperties.setAuthenticateAllArtifacts(true);
-        return serviceProperties;
-    }
-
-    @Bean
-    public CasAuthenticationEntryPoint casAuthenticationEntryPoint(ServiceProperties serviceProperties) {
-        CasAuthenticationEntryPoint entryPoint = new CasAuthenticationEntryPoint();
-        entryPoint.setLoginUrl(this.casServerConfig.getLogin());
-        entryPoint.setServiceProperties(serviceProperties);
-        return entryPoint;
-    }
-
-    @Bean
-    public Cas20ServiceTicketValidator cas20ServiceTicketValidator() {
-        return new Cas30ServiceTicketValidator(this.casServerConfig.getHost());
-    }
-
-    @Bean
-    public CasAuthenticationProvider casAuthenticationProvider(
-        AuthenticationUserDetailsService<CasAssertionAuthenticationToken> userDetailsService,
-        ServiceProperties serviceProperties, Cas20ServiceTicketValidator ticketValidator) {
-        CasAuthenticationProvider provider = new CasAuthenticationProvider();
-        provider.setKey("casProvider");
-        provider.setServiceProperties(serviceProperties);
-        provider.setTicketValidator(ticketValidator);
-        provider.setAuthenticationUserDetailsService(userDetailsService);
-        return provider;
-    }
-
-    @Bean
-    public LogoutFilter logoutFilter() {
-        String logoutRedirectPath = this.casServerConfig.getLogout() + "?service=" + this.casServiceConfig.getHost();
-        LogoutFilter logoutFilter = new LogoutFilter(logoutRedirectPath, new SecurityContextLogoutHandler());
-        logoutFilter.setFilterProcessesUrl(this.casServiceConfig.getLogout());
-        return logoutFilter;
-    }
-
-    @Bean
-    public SingleSignOutFilter singleSignOutFilter() {
-        SingleSignOutFilter singleSignOutFilter = new SingleSignOutFilter();
-        singleSignOutFilter.setCasServerUrlPrefix(this.casServerConfig.getHost());
-        singleSignOutFilter.setIgnoreInitConfiguration(true);
-        return singleSignOutFilter;
-    }
-
-    @Bean
-    public HttpServletRequestWrapperFilter httpServletRequestWrapperFilter() {
-        return new HttpServletRequestWrapperFilter();
     }
 
 }
--- zimu/src/main/java/com/zimu/config/ZimuWebSecurityConfig.java
+++ zimu/src/main/java/com/zimu/config/ZimuWebSecurityConfig.java
@@ -4,10 +4,15 @@ import com.zimu.component.RoleComponent;
 import com.zimu.domain.info.UserInfo;
 import com.zimu.security.UserInfoOauth2UserService;
 import org.jasig.cas.client.session.SingleSignOutFilter;
+import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
 import org.jasig.cas.client.util.HttpServletRequestWrapperFilter;
+import org.jasig.cas.client.validation.Cas30ServiceTicketValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.cas.ServiceProperties;
+import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
 import org.springframework.security.cas.authentication.CasAuthenticationProvider;
 import org.springframework.security.cas.web.CasAuthenticationEntryPoint;
 import org.springframework.security.cas.web.CasAuthenticationFilter;
@@ -16,8 +21,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 import java.util.Iterator;
 import java.util.Map;
@@ -30,28 +39,19 @@ public class ZimuWebSecurityConfig extends WebSecurityConfigurerAdapter {
     private RoleComponent roleComponent;
 
     @Autowired
+    private CasServerConfig casServerConfig;
+
+    @Autowired
     private CasServiceConfig casServiceConfig;
-
-    @Autowired
-    private CasAuthenticationEntryPoint casAuthenticationEntryPoint;
-
-    @Autowired
-    private CasAuthenticationProvider casAuthenticationProvider;
 
     @Autowired
     private UserInfoOauth2UserService userInfoOAuth2UserService;
 
     @Autowired
-    private SingleSignOutFilter singleSignOutFilter;
-
-    @Autowired
-    private LogoutFilter logoutFilter;
-
-    @Autowired
-    private HttpServletRequestWrapperFilter httpServletRequestWrapperFilter;
-
-    @Autowired
     private StaticConfig staticConfig;
+
+    @Autowired
+    private AuthenticationUserDetailsService<CasAssertionAuthenticationToken> authenticationUserDetailsService;
 
     /**
      * 配置信息
@@ -82,12 +82,12 @@ public class ZimuWebSecurityConfig extends WebSecurityConfigurerAdapter {
             .userService(userInfoOAuth2UserService).and().permitAll().and().httpBasic()//
             .and().csrf().disable();
 
-        http.exceptionHandling().authenticationEntryPoint(casAuthenticationEntryPoint)//
+        http.exceptionHandling().authenticationEntryPoint(this.casAuthenticationEntryPoint())//
             .and()//
             .addFilter(this.casAuthenticationFilter())//
-            .addFilterBefore(this.logoutFilter, LogoutFilter.class)//
-            .addFilterBefore(this.singleSignOutFilter, CasAuthenticationFilter.class)//
-            .addFilterAfter(this.httpServletRequestWrapperFilter, CasAuthenticationFilter.class);
+            .addFilterBefore(this.logoutFilter(), LogoutFilter.class)//
+            .addFilterBefore(this.singleSignOutFilter(), CasAuthenticationFilter.class)//
+            .addFilterAfter(new HttpServletRequestWrapperFilter(), CasAuthenticationFilter.class);
     }
 
     /**
@@ -95,11 +95,10 @@ public class ZimuWebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(casAuthenticationProvider);
+        auth.authenticationProvider(this.casAuthenticationProvider());
     }
 
-    @Bean
-    public CasAuthenticationFilter casAuthenticationFilter() throws Exception {
+    private CasAuthenticationFilter casAuthenticationFilter() throws Exception {
         CasAuthenticationFilter casAuthenticationFilter = new CasAuthenticationFilter();
         casAuthenticationFilter.setAuthenticationManager(super.authenticationManager());
         casAuthenticationFilter.setFilterProcessesUrl(this.casServiceConfig.getLogin());
@@ -108,5 +107,54 @@ public class ZimuWebSecurityConfig extends WebSecurityConfigurerAdapter {
         return casAuthenticationFilter;
     }
 
+    private ServiceProperties serviceProperties() {
+        ServiceProperties serviceProperties = new ServiceProperties();
+        serviceProperties.setService(this.casServiceConfig.getHost() + this.casServiceConfig.getLogin());
+        serviceProperties.setSendRenew(this.casServiceConfig.getSendRenew());
+        serviceProperties.setAuthenticateAllArtifacts(true);
+        return serviceProperties;
+    }
+
+    private CasAuthenticationEntryPoint casAuthenticationEntryPoint() {
+        CasAuthenticationEntryPoint entryPoint = new CasAuthenticationEntryPoint();
+        entryPoint.setLoginUrl(this.casServerConfig.getLogin());
+        entryPoint.setServiceProperties(this.serviceProperties());
+        return entryPoint;
+    }
+
+    private CasAuthenticationProvider casAuthenticationProvider() {
+        CasAuthenticationProvider provider = new CasAuthenticationProvider();
+        provider.setKey("casProvider");
+        provider.setServiceProperties(this.serviceProperties());
+        provider.setTicketValidator(new Cas30ServiceTicketValidator(this.casServerConfig.getHost()));
+        provider.setAuthenticationUserDetailsService(authenticationUserDetailsService);
+        return provider;
+    }
+
+    private LogoutFilter logoutFilter() {
+        String logoutRedirectPath = this.casServerConfig.getLogout() + "?service=" + this.casServiceConfig.getHost();
+        LogoutFilter logoutFilter = new LogoutFilter(logoutRedirectPath, new SecurityContextLogoutHandler());
+        logoutFilter.setFilterProcessesUrl(this.casServiceConfig.getLogout());
+        return logoutFilter;
+    }
+
+    private SingleSignOutFilter singleSignOutFilter() {
+        SingleSignOutFilter singleSignOutFilter = new SingleSignOutFilter();
+        singleSignOutFilter.setCasServerUrlPrefix(this.casServerConfig.getHost());
+        singleSignOutFilter.setIgnoreInitConfiguration(true);
+        return singleSignOutFilter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<SingleSignOutHttpSessionListener> addSingleSignOutHttpSessionListener() {
+        ServletListenerRegistrationBean<SingleSignOutHttpSessionListener> servletListenerRegistrationBean = new ServletListenerRegistrationBean<>();
+        servletListenerRegistrationBean.setListener(new SingleSignOutHttpSessionListener());
+        return servletListenerRegistrationBean;
+    }
 
 }
