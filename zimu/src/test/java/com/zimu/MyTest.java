4992e1ebcc7e52707ed14317bf02f4d28eee10d0
yangkun <youngkun@qq.com>
2019-03-20 16:42:39 +0800

    x

---
+++ zimu/src/test/java/com/zimu/MyTest.java
@@ -0,0 +1,9 @@
+package com.zimu;
+
+public class MyTest {
+
+    private static final String COLUMNS_REGEX = "^columns\\[\\d+\\]\\[data\\]$";
+
+    public static void main(String[] args) {
+    }
+}
