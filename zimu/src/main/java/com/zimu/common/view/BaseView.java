d77b0f681fc900cbe4721ef55510be8d3bf08f26
yangkun <youngkun@qq.com>
2019-03-22 11:23:08 +0800

    x

--- zimu/src/main/java/com/zimu/common/view/BaseView.java
+++ zimu/src/main/java/com/zimu/common/view/BaseView.java
@@ -1,6 +1,8 @@
 package com.zimu.common.view;
 
 import com.zimu.common.view.annotation.ViewName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.lang.reflect.Field;
@@ -9,6 +11,8 @@ import java.lang.reflect.Field;
  * @author yk
  */
 public interface BaseView {
+
+    Logger log = LoggerFactory.getLogger(BaseView.class);
 
     /**
      * 构建ModelAndView
@@ -21,8 +25,8 @@ public interface BaseView {
             try {
                 field.setAccessible(true);
                 mv.addObject(field.getName(), field.get(this));
-            } catch (Exception e) {
-
+            } catch (IllegalAccessException e) {
+                log.error(e.getMessage(), e);
             }
         }
         return mv;
