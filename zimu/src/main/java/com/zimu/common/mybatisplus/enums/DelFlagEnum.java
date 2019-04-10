ed389ec522337f656b4cb4807c2f4fd7e0186530
yangkun <youngkun@qq.com>
2019-04-10 13:16:17 +0800

    x

--- zimu/src/main/java/com/zimu/common/mybatisplus/enums/DelFlagEnum.java
+++ zimu/src/main/java/com/zimu/common/mybatisplus/enums/DelFlagEnum.java
@@ -1,4 +1,29 @@
 package com.zimu.common.mybatisplus.enums;
 
-public enum DelFlagEnum {
+import com.baomidou.mybatisplus.core.enums.IEnum;
+
+import java.io.Serializable;
+
+public enum DelFlagEnum implements IEnum {
+
+    /**
+     * 已删除
+     */
+    YES(1),
+
+    /**
+     * 未删除
+     */
+    NO(0);
+
+    private Integer delFlag;
+
+    DelFlagEnum(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    @Override
+    public Serializable getValue() {
+        return delFlag;
+    }
 }
--- zimu/src/main/java/com/zimu/entity/BaseEntity.java
+++ zimu/src/main/java/com/zimu/entity/BaseEntity.java
@@ -1,4 +1,51 @@
 package com.zimu.entity;
 
-public class BaseEntity {
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.Version;
+import com.zimu.common.mybatisplus.enums.DelFlagEnum;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 基础表
+ * </p>
+ *
+ * @author 杨坤
+ * @since 2019-04-10
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField("del_flag")
+    private DelFlagEnum delFlag;
+
+    @TableField("create_by")
+    private String createBy;
+
+    @TableField("create_date")
+    private LocalDateTime createDate;
+
+    @TableField("update_by")
+    private String updateBy;
+
+    @TableField("update_date")
+    private LocalDateTime updateDate;
+
+    @TableField("version")
+    @Version
+    private Integer version;
 }
