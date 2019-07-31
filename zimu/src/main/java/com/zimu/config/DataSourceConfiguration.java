3e850ac3e93dd32b56ba7a81c13d21bcca196c00
yangkun <youngkun@qq.com>
2019-07-31 10:47:35 +0800

    druid

--- zimu/src/main/java/com/zimu/config/DataSourceConfiguration.java
+++ zimu/src/main/java/com/zimu/config/DataSourceConfiguration.java
@@ -1,9 +1,8 @@
 package com.zimu.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,31 +10,20 @@ import org.springframework.context.annotation.Primary;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableConfigurationProperties({DataSourceProperties.class, QuartzDataSourceProperties.class})
 public class DataSourceConfiguration {
 
     @Bean
     @Primary
-    public DataSource zimuDataSource(DataSourceProperties properties) {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setDriverClassName(properties.getDriverClassName());
-        druidDataSource.setUsername(properties.getUsername());
-        druidDataSource.setPassword(properties.getPassword());
-        druidDataSource.setUrl(properties.getUrl());
-        druidDataSource.setName(properties.getName());
-        return druidDataSource;
+    @ConfigurationProperties("spring.datasource.druid.zimu")
+    public DataSource zimuDataSource() {
+        return DruidDataSourceBuilder.create().build();
     }
 
     @Bean
     @QuartzDataSource
-    public DataSource quartzDataSource(QuartzDataSourceProperties properties) {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setDriverClassName(properties.getDriverClassName());
-        druidDataSource.setUsername(properties.getUsername());
-        druidDataSource.setPassword(properties.getPassword());
-        druidDataSource.setUrl(properties.getUrl());
-        druidDataSource.setName(properties.getName());
-        return druidDataSource;
+    @ConfigurationProperties("spring.datasource.druid.quartz")
+    public DataSource quartzDataSource() {
+        return DruidDataSourceBuilder.create().build();
     }
 
 }
